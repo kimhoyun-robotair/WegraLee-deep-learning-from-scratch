ec35453101b84017c662516f1b9f62e5d764efbd
root <root@DESKTOP-AN2F15B>
2025-04-05 10:54:02 +0900

    Small Change

--- ch04/gradient_2d.py
+++ ch04/gradient_2d.py
@@ -8,21 +8,21 @@ from mpl_toolkits.mplot3d import Axes3D
 def _numerical_gradient_no_batch(f, x):
     h = 1e-4 # 0.0001
     grad = np.zeros_like(x) # x와 형상이 같은 배열을 생성
-    
+
     for idx in range(x.size):
         tmp_val = x[idx]
-        
+
         # f(x+h) 계산
         x[idx] = float(tmp_val) + h
         fxh1 = f(x)
-        
+
         # f(x-h) 계산
-        x[idx] = tmp_val - h 
-        fxh2 = f(x) 
-        
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
         grad[idx] = (fxh1 - fxh2) / (2*h)
         x[idx] = tmp_val # 값 복원
-        
+
     return grad
 
 
@@ -31,10 +31,10 @@ def numerical_gradient(f, X):
         return _numerical_gradient_no_batch(f, X)
     else:
         grad = np.zeros_like(X)
-        
+
         for idx, x in enumerate(X):
             grad[idx] = _numerical_gradient_no_batch(f, x)
-        
+
         return grad
 
 
@@ -50,17 +50,17 @@ def tangent_line(f, x):
     print(d)
     y = f(x) - d*x
     return lambda t: d*t + y
-     
+
 if __name__ == '__main__':
     x0 = np.arange(-2, 2.5, 0.25)
     x1 = np.arange(-2, 2.5, 0.25)
     X, Y = np.meshgrid(x0, x1)
-    
+
     X = X.flatten()
     Y = Y.flatten()
-    
+
     grad = numerical_gradient(function_2, np.array([X, Y]) )
-    
+
     plt.figure()
     plt.quiver(X, Y, -grad[0], -grad[1],  angles="xy",color="#666666")#,headwidth=10,scale=40,color="#444444")
     plt.xlim([-2, 2])
